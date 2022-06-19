9b96a5f52b12947d60722b9edfa62d3c9239f0b9
ikechukwu mmachukwu <ikechukwummachukwu2020@gmail.com>
2022-06-19 12:06:11 +0100

    minor changes

--- I4G004770XII/blog/models.py
+++ I4G004770XII/blog/models.py
@@ -1,9 +1,11 @@
+from tkinter import CASCADE
+from typing_extensions import Self
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField
-    Author = models.CharField(max_length=200)
+    Author = models.ForeignKey(to=Self, on_delete=models.CASCADE)
     created_date = models.DateTimeField
     published_date = models.DateTimeField
